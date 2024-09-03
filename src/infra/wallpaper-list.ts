860fd7b2d11d29326eeb6687118eac57b09ece8a
Ketryn Silva <78522164+ketrynsdm@users.noreply.github.com>
2024-09-02 21:01:54 -0300

    list

--- src/infra/wallpaper-list.ts
+++ src/infra/wallpaper-list.ts
@@ -3,9 +3,11 @@ import { WallpaperItem } from '@/domain';
 import { demonSlayerList } from './demon-slayer-list';
 import { bokuNoHeroList } from './boku-no-hero-list';
 import { jujutsuList } from './jujutsu-kaisen-list';
+import { narutoList } from './naruto-list';
 
 export const wallpaperList: WallpaperItem[] = [
   ...demonSlayerList,
   ...bokuNoHeroList,
   ...jujutsuList,
+  ...narutoList
 ];
